e2f3222f502025574e3f395cbefe22676bcef070
Ryant <bibarra.dev@gmail.com>
2026-05-11 23:05:25 -0500

    Creacion de login y metodo abstracto para empezar con aplicativo y preparar el log

--- SoftwareFJ.py
+++ SoftwareFJ.py
@@ -1,1 +1,10 @@
 
+import logging
+from abc import ABC, abstractmethod
+
+# Preparar log
+logging.basicConfig(
+    filename='errores_sistema.log',
+    level=logging.ERROR,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
